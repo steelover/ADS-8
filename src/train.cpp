feafe50dc0a2d859457737e787a929d2b1985d15
steelover <150594943+steelover@users.noreply.github.com>
2024-05-13 22:04:04 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,23 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() {}
+
+void Train::addCage(bool light) {
+  arr[last] = light;
+  last += 1;
+  if (!check) {
+    check = true;
+    start = light;
+  }
+}
+
+int Train::getLength() { return last; }
+
+int Train::getOpCount() {
+  if (start) {
+    return last * last + last;
+  } else {
+    return last * 2;
+  }
+}
